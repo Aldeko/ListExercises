77e56bd9d808d2ef72e76d191f82751ceed822b4
Unai Aldekozea <unaizeni@hotmail.com>
2018-10-18 11:06:07 +0200

    EjercicioFinalizado

---
+++ ExerciseList 4/ExerciseList 4/CantanteFamoso.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseList_4
+{
+    class CantanteFamoso
+    {
+        private string nombre, discoConMasVentas;
+
+        public CantanteFamoso ()
+        {
+            nombre = "YLVIS";
+            discoConMasVentas = "WHAT DOES THE FOX SAY";
+
+        }
+        public CantanteFamoso(string nombre, string discoConMasVentas)
+        {
+            this.nombre = nombre;
+            this.discoConMasVentas = discoConMasVentas;
+        }
+        public string Getnombre()
+        {
+            return nombre;
+        }
+        public void Setnombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string GetDiscoConMasventas()
+        {
+            return discoConMasVentas;
+        }
+        public void SetDiscoConMasVEntas(string discoConMasVentas)
+        {
+            this.discoConMasVentas = discoConMasVentas;
+        }
+    }
+
+}
---
+++ ExerciseList 4/ExerciseList 4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseList_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cantante, disco, salida;
+
+            CantanteFamoso ca1 = new CantanteFamoso("GATIBU", "Laino gustien gainetik");
+            CantanteFamoso ca2 = new CantanteFamoso("MICHAEL JACKSON", "Man on the mirror");
+
+            List<CantanteFamoso> listaCantantesFamosos = new List<CantanteFamoso>();
+            listaCantantesFamosos.Add(ca1);
+            listaCantantesFamosos.Add(ca2);
+
+            foreach (CantanteFamoso  x in listaCantantesFamosos)
+            {
+                Console.Write(x.Getnombre() + " =====>");
+                Console.WriteLine(x.GetDiscoConMasventas());
+
+            }
+            do
+            {
+
+
+                do
+                {
+
+                    Console.WriteLine("escribe un nombre");
+                    cantante = Console.ReadLine();
+                    Console.WriteLine("Escribe un disco");
+                    disco = Console.ReadLine();
+
+                    if (cantante != "0" && disco != "0")
+                    {
+                        listaCantantesFamosos.Add(new CantanteFamoso(cantante, disco));
+                    }
+
+                } while (cantante != "0" || disco != "0");
+
+                foreach (CantanteFamoso x in listaCantantesFamosos)
+                {
+                    Console.Write(x.Getnombre() + " =====>");
+                    Console.WriteLine(x.GetDiscoConMasventas());
+                }
+
+                Console.WriteLine("Quieres meter mas datos? (S/N)");
+                salida = Console.ReadLine();
+            } while (salida != "N");
+
+                Console.ReadLine();
+
+        }
+    }
+}
